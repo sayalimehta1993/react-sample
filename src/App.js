d8110d2a909c34caebd4b99fdb7bafa71ec3352d
Sayali <sayalivetal@gmail.com>
2021-09-30 10:43:21 +0530

    remove container

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ class App extends Component {
     return (
       <div>
         <NavBar />
-        <Container>
+      
           <TabsData />
-        </Container>
+      
       </div>
     );
   }
